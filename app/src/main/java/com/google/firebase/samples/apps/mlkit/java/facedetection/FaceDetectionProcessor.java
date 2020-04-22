877b2a3ca80ed95be0f0200b6957a6f6b6b575f7
Jose Ribeiro <jose.ribeiro@ipleiria.pt>
2020-04-22 16:06:58 +0100

    Initial commit

---
+++ app/src/main/java/com/google/firebase/samples/apps/mlkit/java/facedetection/FaceDetectionProcessor.java
@@ -0,0 +1,122 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.firebase.samples.apps.mlkit.java.facedetection;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.google.firebase.samples.apps.mlkit.R;
+import com.google.firebase.samples.apps.mlkit.common.CameraImageGraphic;
+import com.google.firebase.samples.apps.mlkit.common.FrameMetadata;
+import com.google.firebase.samples.apps.mlkit.common.GraphicOverlay;
+import com.google.firebase.samples.apps.mlkit.java.LivePreviewActivity;
+import com.google.firebase.samples.apps.mlkit.java.VisionProcessorBase;
+
+import java.io.IOException;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * Face Detector Demo.
+ */
+public class FaceDetectionProcessor extends VisionProcessorBase<List<FirebaseVisionFace>> {
+
+    private static final String TAG = "FaceDetectionProcessor";
+
+    private final FirebaseVisionFaceDetector detector;
+
+    private final Bitmap overlayBitmap;
+
+    public FaceDetectionProcessor(Resources resources) {
+        FirebaseVisionFaceDetectorOptions options =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .build();
+
+        detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
+
+        overlayBitmap = BitmapFactory.decodeResource(resources, R.drawable.clown_nose);
+    }
+
+    @Override
+    public void stop() {
+        try {
+            detector.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Exception thrown while trying to close Face Detector: " + e);
+        }
+    }
+
+    @Override
+    protected Task<List<FirebaseVisionFace>> detectInImage(FirebaseVisionImage image) {
+        return detector.detectInImage(image);
+    }
+
+    @Override
+    protected void onSuccess(
+            @Nullable Bitmap originalCameraImage,
+            @NonNull List<FirebaseVisionFace> faces,
+            @NonNull FrameMetadata frameMetadata,
+            @NonNull GraphicOverlay graphicOverlay) {
+        graphicOverlay.clear();
+        if (originalCameraImage != null) {
+            CameraImageGraphic imageGraphic = new CameraImageGraphic(graphicOverlay, originalCameraImage);
+            graphicOverlay.add(imageGraphic);
+        }
+        for (int i = 0; i < faces.size(); ++i) {
+            FirebaseVisionFace face = faces.get(i);
+
+          // NOTE example of how to communicate with the UI thread
+          if (face.getSmilingProbability() < 0.3) {
+            Message message = LivePreviewActivity.mHandler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("FEEDBACK", ":(  Why so serious?");
+            message.setData(bundle);
+            LivePreviewActivity.mHandler.sendMessage(message);
+          } else if (face.getSmilingProbability() > 0.7) {
+            Message message = LivePreviewActivity.mHandler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("FEEDBACK", ":)  Let's put a smile on that face!");
+            message.setData(bundle);
+            LivePreviewActivity.mHandler.sendMessage(message);
+          }
+
+            int cameraFacing =
+                    frameMetadata != null ? frameMetadata.getCameraFacing() :
+                            Camera.CameraInfo.CAMERA_FACING_BACK;
+            FaceGraphic faceGraphic = new FaceGraphic(graphicOverlay, face, cameraFacing, overlayBitmap);
+            graphicOverlay.add(faceGraphic);
+        }
+        graphicOverlay.postInvalidate();
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.e(TAG, "Face detection failed " + e);
+    }
+}
